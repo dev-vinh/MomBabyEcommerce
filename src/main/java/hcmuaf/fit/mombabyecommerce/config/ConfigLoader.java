e313f2df6248cba88048eea9c3a9e6f498908e9e
dev_vinh <23130385@st.hcmuaf.edu.vn>
2026-03-10 10:30:36 +0700

    feat-connectiondb-kết nối db và biến evn

---
+++ src/main/java/hcmuaf/fit/mombabyecommerce/config/ConfigLoader.java
@@ -0,0 +1,22 @@
+package hcmuaf.fit.mombabyecommerce.config;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigLoader {
+    private static Properties properties;
+
+    static {
+        properties = new Properties();
+        try {
+            properties.load(ConfigLoader.class.getClassLoader().getResourceAsStream("application.properties"));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String get(String key) {
+        return properties.getProperty(key);
+    }
+}
