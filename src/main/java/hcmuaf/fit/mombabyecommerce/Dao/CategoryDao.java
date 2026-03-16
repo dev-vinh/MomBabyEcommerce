4f29cf84ae84cc9257db91a78ffb17fd94c8ff29
Dong Chi <21130295@st.hcmuaf.edu.vn>
2026-03-16 15:36:13 +0700

    feat - admin-category - khỏi tạo giao diện  danh mục và lấy ra danh sách danh mục SP

---
+++ src/main/java/hcmuaf/fit/mombabyecommerce/Dao/CategoryDao.java
@@ -0,0 +1,34 @@
+package hcmuaf.fit.mombabyecommerce.Dao;
+
+import hcmuaf.fit.mombabyecommerce.model.Category;
+import org.jdbi.v3.sqlobject.config.RegisterConstructorMapper;
+import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.statement.GetGeneratedKeys;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+
+import java.util.List;
+
+@RegisterConstructorMapper(Category.class)
+public interface CategoryDao {
+
+    @SqlQuery("SELECT * FROM categories WHERE isActive = 1")
+    List<Category> getAllCategories();
+
+    @SqlQuery("SELECT * FROM categories WHERE id = :id")
+    Category getCategoryById(@Bind("id") Integer id);
+
+    @SqlUpdate("INSERT INTO categories (name, isActive) VALUES (:name, COALESCE(:isActive, 1))")
+    @GetGeneratedKeys("id")
+    int createCategory(@Bind("name") String name, @Bind("isActive") Boolean isActive);
+
+
+    @SqlUpdate("UPDATE categories SET name = :name WHERE id = :id")
+    void updateCategory(@Bind("id") Integer id, @Bind("name") String name);
+
+    @SqlUpdate("DELETE FROM categories WHERE id = :id")
+    void deleteCategory(@Bind("id") Integer id);
+
+
+
+}
---
+++ src/main/java/hcmuaf/fit/mombabyecommerce/service/CategoryService.java
@@ -0,0 +1,47 @@
+package hcmuaf.fit.mombabyecommerce.service;
+
+import hcmuaf.fit.mombabyecommerce.Dao.CategoryDao;
+import hcmuaf.fit.mombabyecommerce.model.Category;
+import org.jdbi.v3.core.Jdbi;
+
+import java.util.List;
+
+public class CategoryService {
+    private final CategoryDao categoryDao;
+
+    public CategoryService(Jdbi jdbi) {
+        this.categoryDao = jdbi.onDemand(CategoryDao.class);
+    }
+
+
+
+
+    public List<Category> getAllCategories() {
+        return categoryDao.getAllCategories();
+    }
+
+
+
+    public Category getCategoryById(Integer id) {
+        Category category = categoryDao.getCategoryById(id);
+        if (category == null) {
+            throw new IllegalArgumentException("Category not found");
+        }
+        return category;
+    }
+
+    public Category createCategory(String name, Boolean isActive ) {
+        int id = categoryDao.createCategory(name, isActive);
+        return categoryDao.getCategoryById(id);
+    }
+
+    public void updateCategory(Integer id, String name) {
+        categoryDao.updateCategory(id, name);
+    }
+
+    public void deleteCategory(Integer id) {
+        categoryDao.deleteCategory(id);
+    }
+
+
+}
