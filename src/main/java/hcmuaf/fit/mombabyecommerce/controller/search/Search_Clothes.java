756c5e593d4956ed9ee64e4e92d8fa3218fe2b23
quang tuan <23130371@st.hcmuaf.edu.vn>
2026-03-21 14:28:17 +0700

    feature - chức năng hiển thị, tìm kiếm theo từng danh mục - thêm controller search ăn dặm, bỉm tã, trang phục, ...

---
+++ src/main/java/hcmuaf/fit/mombabyecommerce/controller/search/Search_Clothes.java
@@ -0,0 +1,37 @@
+package hcmuaf.fit.mombabyecommerce.controller.search;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import hcmuaf.fit.mombabyecommerce.connection.DBConnection;
+import hcmuaf.fit.mombabyecommerce.model.Product;
+import hcmuaf.fit.mombabyecommerce.service.ProductService;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "Search_Clothes", value = "/Search_Clothes")
+public class Search_Clothes extends HttpServlet {
+    ProductService productService = new ProductService(DBConnection.getJdbi());
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        List<Product> products = productService.getProductsByCategory(5);
+        List<Product> topProducts = productService.getTopProductsByCategory(5, 4);
+
+        request.setAttribute("products", products);
+        request.setAttribute("topProducts", topProducts);
+        request.setAttribute("categoryId", 5);
+
+        request.getRequestDispatcher("search/search-clothings.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Xử lý yêu cầu POST ở đây
+    }
+}
