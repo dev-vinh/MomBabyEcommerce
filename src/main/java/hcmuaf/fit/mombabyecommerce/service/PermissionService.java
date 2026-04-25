633a37031394d3511e791723e2a280784a11ca2b
dev_vinh <23130385@st.hcmuaf.edu.vn>
2026-04-25 10:38:25 +0700

    feat-phân quyền-admin phân cấp quyền cho tài khoản user

---
+++ src/main/java/hcmuaf/fit/mombabyecommerce/service/PermissionService.java
@@ -0,0 +1,26 @@
+package hcmuaf.fit.mombabyecommerce.service;
+
+import hcmuaf.fit.mombabyecommerce.Dao.PermissionDao;
+import hcmuaf.fit.mombabyecommerce.connection.DBConnection;
+import hcmuaf.fit.mombabyecommerce.model.Permission;
+import org.jdbi.v3.core.Jdbi;
+
+import java.util.List;
+
+public class PermissionService {
+    private final PermissionDao permissionDAO;
+
+    public PermissionService(Jdbi jdbi) {
+        this.permissionDAO = jdbi.onDemand(PermissionDao.class);
+    }
+
+    public List<Permission> getAllPermissions() {
+        return permissionDAO.getAllPermissions();
+    }
+
+    public static void main(String[] args) {
+        PermissionService permissionService = new PermissionService(DBConnection.getJdbi());
+        List<Permission> permissions = permissionService.getAllPermissions();
+        System.out.println(permissions);
+    }
+}
